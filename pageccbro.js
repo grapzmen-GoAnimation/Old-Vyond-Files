81e24e2a0a12f989e4d1174d97d54fbc72ae9e23
grapzmen-GoAnimation <arthurhenriqueesr@gmail.com>
2025-10-17 16:14:50 -0300

    Refactor HTML structure and update URLs
    Updated various elements in the HTML structure, including links and script sources, to reflect new URLs and improve organization. Adjusted the title and removed deprecated comments.

--- pageccbro.js
+++ pageccbro.js
@@ -1,6 +1,4 @@
-// this file is for the static folder in wrapper offline 1.2.3.
-const sessions = require('../data/sessions');
-const fUtil = require('../fileUtil');
+const fUtil = require('../misc/file');
 const stuff = require('./info');
 
 function toAttrString(table) {
@@ -25,18 +23,17 @@ module.exports = function (req, res, url) {
 	var attrs, params, title;
 	switch (url.pathname) {
 		case '/charactercreator/': {
-			title = 'The Character Creator from Vyond - Make a character online';
+			title = 'The Character Creator from Vyond';
 			attrs = {	
 				data: process.env.SWF_URL + '/cc_browser.swf', // data: 'cc.swf',
 				type: 'application/x-shockwave-flash', id: 'char_creator', width: '960', height: '1200',
 			};
 			params = {
 				flashvars: {
-					'apiserver': '/', 'isEmbed': '0', 'ctc': 'go', 'tlang': 'en_US', 'storePath':  process.env.STORE_URL + '/<store>',
-					'clientThemePath': process.env.CLIENT_URL + '/<client_theme>', 'appCode': 'go', 'siteId': 'school', 'st': '', 
-					'userId': '0DyHqK6Yj9dM', 'ut': 23, 'uisa': 0, 'themeId': 'family',
-					'u_info_school': 'OjI6c2xoVVM3MWJIX05DMnA4cmRBQ2dFd3JvNE5xc2JEc2o4UFB2X1dVd2Eya2RPQisxVTl4d3ZHZHJPYnI4QURFNENWMjNkYm12WFdlUGxLT0g0OG4rekF5ajZhWGRGVTlocmJ4S1JhSWhCVXJlTF9BbXdyQUp3PQ==',
-
+					'apiserver': '/', 'storePath': process.env.STORE_URL + '/<store>',
+					'clientThemePath': process.env.CLIENT_URL + '/<client_theme>',
+					'themeId': 'family', 'ut': 60, 'appCode': 'go', 'page': '', 'siteId': 'go',
+					'm_mode': 'school', 'isLogin': 'Y', 'isEmbed': 1, 'ctc': 'go', 'tlang': 'en_US',
 				},
 				allowScriptAccess: 'always',
 				movie: process.env.SWF_URL + '/cc.swf', // 'http://localhost/cc.swf'
@@ -52,16 +49,18 @@ module.exports = function (req, res, url) {
 	`<html><head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<link rel="dns-prefetch" href="https://josephcrosmanplays532.github.io">
+<link rel="dns-prefetch" href="https://d2bm7x1jqouzel.cloudfront.net">
+<link rel="dns-prefetch" href="//d3v4eglovri8yt.cloudfront.net">
 <script>document.title='${title}'</script>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="The Character Creator from Vyond - Create a character online with Vyond.">
 <meta property="og:site_name" content="Vyond">
 <meta property="fb:app_id" content="177116303202">
 <meta name="google-site-verification" content="K_niiTfCVi72gwvxK00O4NjsVybMutMUnc-ZnN6HUuA">
-<link rel="canonical" href="https://localhost:4343/pages/html/list.html">
-<link href="/pages/css/global.css" rel="stylesheet" type="text/css">
-<link href="https://josephcrosmanplays532.github.io/static/55910a7cd204c37c/go/css/cc.css.gz.css" rel="stylesheet" type="text/css">
+<link rel="canonical" href="https://vyondremastered1.herokuapp.com/yourvideos">
+<link href="https://josephanimate2021.github.io/fonts/1/sailec.css" rel="stylesheet" type="text/css">
+<link href="https://josephanimate2021.github.io/static/55910a7cd204c37c/go/css/common_combined.css.gz.css" rel="stylesheet" type="text/css">
+<link href="https://josephanimate2021.github.io/static/55910a7cd204c37c/go/css/cc.css.gz.css" rel="stylesheet" type="text/css">
 <!--[if lt IE 9]>
 <style text="text/css">
 .top-nav.collapse {height: auto;overflow: visible;}
@@ -71,14 +70,14 @@ module.exports = function (req, res, url) {
 var srv_tz_os = -4, view_name = "go", user_cookie_name = "u_info";
 var user_role = 11;
 </script>
-<script src="https://josephcrosmanplays532.github.io/static/55910a7cd204c37c/go/js/common_combined.js.gz.js"></script>
-<script type="text/javascript" src="https://josephcrosmanplays532.github.io/static/55910a7cd204c37c/go/js/../po/goserver_js-en_US.json.gz.json"></script>
+<script src="https://josephanimate2021.github.io/static/55910a7cd204c37c/go/js/common_combined.js.gz.js"></script>
+<script type="text/javascript" src="https://josephanimate2021.github.io/static/55910a7cd204c37c/go/js/../po/goserver_js-en_US.json.gz.json"></script>
 <script type="text/javascript">
 var I18N_LANG = 'en_US';
 var GT = new Gettext({'locale_data': json_locale_data});
 </script>
-<script src="https://josephcrosmanplays532.github.io/static/55910a7cd204c37c/go/js/sessionChecker.js.gz.js"></script>
-<script src="https://josephcrosmanplays532.github.io/static/55910a7cd204c37c/go/js/amplitude/go_amp.js.gz.js"></script>
+<script src="https://josephanimate2021.github.io/static/55910a7cd204c37c/go/js/sessionChecker.js.gz.js"></script>
+<script src="https://josephanimate2021.github.io/static/55910a7cd204c37c/go/js/amplitude/go_amp.js.gz.js"></script>
 <!-- Vyond Cookie Consent -->
 <script>(function(v,y,o,n){v[n]=v[n]||[];
 var f=y.getElementsByTagName(o)[0],d=y.createElement(o);
@@ -112,109 +111,138 @@ if (self !== top) {
         jQuery.extend(CCStandaloneBannerAdUI, {"actionshopSWF":"https:\/\/d3v4eglovri8yt.cloudfront.net\/animation\/66453a3ba2cc5e1b\/actionshop.swf","apiserver":"http:\/\/ga.vyond.com\/","clientThemePath":"https:\/\/d3v4eglovri8yt.cloudfront.net\/static\/55910a7cd204c37c\/<client_theme>","userId":"0TBAAga2Mn6g"});
 </script>
 <div class="page-container">
-<header id="header">
-		<a href="/">
-			<h1 style="margin:0"><img id="logo" src="/pages/img/list_logo.svg" alt="Wrapper: Offline"/></h1>
-		</a>
-		<nav id="headbuttons">
-			<div class="dropdown_contain button_small">
-				<div class="dropdown_button upload_button">UPLOAD</div>
-				<nav class="dropdown_menu">
-					<a onclick="document.getElementById('file').click()">Movie</a>
-					<a onclick="document.getElementById('file2').click()">Character</a>
-				</nav>
-			</div>	
-			<div class="dropdown_contain button_small">
-				<div class="dropdown_button">CREATE A CHARACTER</div>
-				<nav class="dropdown_menu">
-					<h2>Comedy World</h2>
-					<a href="/cc?themeId=family&bs=adam">Guy (Adam)</a>
-					<a href="/cc?themeId=family&bs=eve">Girl (Eve)</a>
-					<a href="/cc?themeId=family&bs=bob">Fat (Bob)</a>
-					<a href="/cc?themeId=family&bs=rocky">Buff (Rocky)</a>
-					<hr />
-					<h2>Anime</h2>
-					<a href="/cc?themeId=anime&bs=guy">Guy</a>
-					<a href="/cc?themeId=anime&bs=girl">Girl</a>
-					<a href="/cc?themeId=ninjaanime&bs=guy">Guy (Ninja)</a>
-					<a href="/cc?themeId=ninjaanime&bs=girl">Girl (Ninja)</a>
-					<hr />
-					<h2>Peepz</h2>
-					<a href="/cc?themeId=cc2&bs=default">Lil Peepz</a>
-					<a href="/cc?themeId=chibi&bs=default">Chibi Peepz</a>
-					<a href="/cc?themeId=ninja&bs=default">Chibi Ninjas</a>
-				</nav>
-			</div>
-			<div class="dropdown_contain button_small">
-				<div class="dropdown_button">BROWSE CHARACTERS</div>
-				<nav class="dropdown_menu">
-					<h2>Comedy World</h2>
-					<a href="/cc_browser?themeId=family">Comedy World</a>
-					<hr />
-					<h2>Anime</h2>
-					<a href="/cc_browser?themeId=anime">Anime</a>
-					<a href="/cc_browser?themeId=ninjaanime">Ninja Anime</a>
-					<hr />
-					<h2>Peepz</h2>
-					<a href="/cc_browser?themeId=cc2">Lil' Peepz</a>
-					<a href="/cc_browser?themeId=chibi">Chibi Peepz</a>
-					<a href="/cc_browser?themeId=ninja">Chibi Ninjas</a>
-				</nav>
-			</div>
-			<a href="/go_full" class="button_big">MAKE A VIDEO</a>
-		</nav>
-	</header>
+<div class="site-header">
+    <div class="navbar site-nav site-nav--legacy" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                  <a class="navbar-brand" href="/yourvideos" title="Vyond">
+                      <img alt="Vyond" src="/pages/html/logo.png">
+                  </a>
+            </div>
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav navbar-right">
+<li class="dropdown">
+     <a class="dropdown-toggle" href="https://josephanimate2021.github.io/help" data-toggle="dropdown">Help<span class="dropdown-caret"></span></a>
+    <ul class="dropdown-menu dropdown-menu-help">
+
+        <li>
+            <a href="#" onclick="zE.activate({hideOnClose: true});amplitudeTrackCtaHelp('quick_search');return false;">Quick Search</a>
+        </li>
+        <li>
+            <a href="https://josephanimate2021.github.io/help" onclick="amplitudeTrackCtaHelp('help_center');" target="_blank">Help Center</a>
+        </li>
+        <li>
+            <a href="https://josephanimate2021.github.io/whatsnew" onclick="amplitudeTrackCtaHelp('whats_new');" target="_blank">What’s New</a>
+        </li>
+    </ul>
+</li>
+
+         <li>
+                        <a class="hidden-sm hidden-md hidden-lg" href="/videomaker">Make a Video</a>
+                        <span class="site-nav-btn hidden-xs"><a class="btn btn-orange" href="/pages/html/ga.vyond.com/videomaker.html">Make a Video</a></span>
+                    </li>
+<li class="dropdown">
+    <a class="dropdown-toggle" href="/account" data-toggle="dropdown" aria-expanded="false">
+        <span class="hidden-sm hidden-md hidden-lg">Your Account</span>
+        <div class="site-nav__profile-image">
+            <div class="badge-circle">U</div>
+        </div><span class="dropdown-caret"></span>
+    </a>
+    <ul class="dropdown-menu dropdown-menu-user">
+        <li class="dropdown-user-profile">
+            <div class="dropdown-user-profile__display-name">
+                User 1            </div>
+            <div class="dropdown-user-profile__status">
+                Free Trial | <a href="https://www.vyond.com/pricing">Upgrade now</a>            </div>
+        </li>
+        <li class="divider"></li>
+        <li><a href="https://app.vyond.com/v2/profile">Profile Settings</a></li>
+        <li><a href="https://app.vyond.com/v2/users/list">Users</a></li>
+        <li><a href="https://app.vyond.com/v2/security">Security</a></li>
+        <li><a href="https://app.vyond.com/v2/subscription">Subscription</a></li>
+        <li class="divider"></li>
+        <li><a href="https://vyondstudio.herokuapp.com/video/list" onclick="amplitudeTrackSwitchVideoMaker('Go to Vyond Studio')">Go to Vyond Studio</a></li>
+        <li class="divider"></li>
+        <li><a class="gtm-logout" href="https://www.vyond.com/">Log Out</a></li>
+    </ul>
+</li>
+
+                </ul>
+            </div>
+    </div>
+<script src="https://josephanimate2021.github.io/video-player-2015-files/users.js"></script>
+<script src="https://josephanimate2021.github.io/video-player-2015-files/video.js"></script>
+<script src="https://josephanimate2021.github.io/video-player-2015-files/videos.js"></script>
+ You can't use Vyond because Flash might be disabled. <a href="https://sites.google.com/view/wrapperstoreremastered/requirements/">Enable Flash</a>.
+</div>
+
 <div class="container container-cc">
+
         <ul class="breadcrumb">
-	<a href="/go_full">Make a video</a>  >  <a class="active">Your Characters</a>
+            <li><a href="/pages/html/ga.vyond.com/videomaker.html">Make a video</a></li>
+            <li class="active">Whiteboard Animation Characters</li>
         </ul>
+
         <p>Browse characters already available in that theme and use them as a starting point to create new custom characters.</p>
+
 <div id="ccbrowser-container" align="center">${toObjectString(attrs, params)}</div>
+
 <script>
+
 jQuery('#ccbrowser-container').flash({
     id: "ccbrowser",
-    swf: "https://localhost:4664/animation/414827163ad4eb60/cc_browser.swf",
+    swf: "https://josephanimate2021.github.io/animation/cce25167cb1d3404/cc_browser.swf",
     height: 1200,
     width: 960,
+
     align: "middle",
     allowScriptAccess: "always",
     allowFullScreen: "true",
     wmode: "transparent",
+
     hasVersion: "10.3",
-    flashvars: ${JSON.stringify(params.flashvars)}});
+
+    flashvars: ${JSON.stringify(params.flashvars)}
+
 </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
 </div>
-<form enctype='multipart/form-data' action='/upload_movie' method='post'>
-<input id='file' type="file" onchange="this.form.submit()" name='import' />
-</form>
-<form enctype='multipart/form-data' action='/upload_character' method='post'>
-	<input id='file2' type="file" onchange="this.form.submit()" name='import' />
-</form>
-<footer>
-	<nav id="foot-left">
-		<a>v1.2.3½</a>
-		<a href="https://localhost:4664/faq.html">FAQ</a>
-		<a href="https://discord.gg/yhGAetN">Wrapper Hub</a>
-	</nav>
-	<nav id="foot-right">
-		<a href="https://localhost:4664">Server Page</a>
-		<a href="https://localhost:4664/caillou.html">Caillou</a>
-	</nav>
+
+<footer class="site-footer">
+    <div class="container">
+        Vyond™ is a trademark of GoAnimate. Inc. ©2018 GoAnimate. Inc. <a href="https://www.vyond.com/terms">Terms of Service</a> | <a href="https://www.vyond.com/privacy">Privacy Policy</a> | <a href="https://www.vyond.com/cookies">Cookies Policy</a>
+    </div>
+
 </footer>
 <div id="studio_container" style="display: none;">
     <div id="studio_holder"><!-- Full Screen Studio -->
         <div style="top: 50%; position: relative;">
-            You can't use GoAnimate because Flash might be disabled. <a href="https://get.adobe.com/flashplayer/">Enable Flash</a>.
+            You can't use GoAnimate because Flash might be disabled. <a href="https://web.archive.org/web/20191022104022/https://get.adobe.com/flashplayer/">Enable Flash</a>.
         </div>
     </div>
 </div>
+
+
+
 </body>
-</html>
-<style>
-	form {
-			display: none;
-		}
-</style>`
+</html>`
 		);
 	return true;
 }
